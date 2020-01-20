36f5529c4a80a16c09c2daf049e52047c9661623
hbarang <hbarangenc@gmail.com>
2020-01-20 16:23:40 +0300

    add UI elements

---
+++ Assets/UIManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+    public Slider forceSlider;
+    public void RestartGame()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+
+    public void SetMagneticConstant(){
+        Utility.Instance.magneticForceConstant = forceSlider.value;
+    }
+
+}
