525f9c7fde20207b7b09d79293f7dc8bd8effdc2
hbarang <hbarangenc@gmail.com>
2020-01-20 20:33:54 +0300

    polarizer responsive position

---
+++ Assets/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public Transform polarizationChangerNegative;
+    public Transform polarizationChangerPositive;
+
+    void Start()
+    {
+        Boundaries.Instance.OnScreenBoundsChangeEvent += AdjustPolarizerPosition;
+
+        AdjustPolarizerPosition();
+
+    }
+
+    void AdjustPolarizerPosition()
+    {
+        polarizationChangerNegative.position = new Vector3(Boundaries.Instance.ScreenBounds.max.x, 0.1f, -Boundaries.Instance.ScreenBounds.extents.y);
+        polarizationChangerPositive.position = new Vector3(Boundaries.Instance.ScreenBounds.min.x, 0.1f, Boundaries.Instance.ScreenBounds.extents.y);
+    }
+
+}
