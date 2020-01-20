65b01dc02bbeb6c1caf3f1d47519279aed9324cc
hbarang <hbarangenc@gmail.com>
2020-01-20 13:56:20 +0300

    add border controller for responsive screen

---
+++ Assets/BorderController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BorderController : MonoBehaviour
+{
+
+
+    public Transform[] borders;
+
+    void Start()
+    {
+
+        AdjustPosition();
+        Boundaries.Instance.OnScreenBoundsChangeEvent += AdjustPosition;
+
+    }
+    void AdjustPosition()
+    {
+
+        for (int i = 0; i < borders.Length; i++)
+        {
+            if (i < 2)
+                HandleHorizontal(borders[i], i == 0);
+            else
+                HandleVertical(borders[i], i == 2);
+        }
+
+    }
+
+    void HandleHorizontal(Transform border, bool isWest)
+    {
+        Vector3 newPosition = new Vector3((isWest ? 1 : -1) * Boundaries.Instance.ScreenBounds.min.x, border.position.y, Boundaries.Instance.ScreenBounds.center.y);
+        border.position = newPosition;
+    }
+    void HandleVertical(Transform border, bool isNorth)
+    {
+        Vector3 newPosition = new Vector3(0, border.position.y, (isNorth ? 1 : -1) * Boundaries.Instance.ScreenBounds.min.y);
+        border.position = newPosition;
+    }
+
+    private void OnDestroy()
+    {
+
+        Boundaries.Instance.OnScreenBoundsChangeEvent -= AdjustPosition;
+
+    }
+}
---
+++ Assets/Boundaries.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundaries : MonoBehaviour
+{
+
+    Bounds _screenBounds;
+    public Bounds ScreenBounds
+    {
+        get
+        {
+            return _screenBounds;
+        }
+        set
+        {
+            _screenBounds = value;
+
+            if (OnScreenBoundsChangeEvent != null)
+            {
+                OnScreenBoundsChangeEvent();
+            }
+        }
+    }
+
+    public delegate void OnScreenBoundsChange();
+    public event OnScreenBoundsChange OnScreenBoundsChangeEvent;
+    public static Boundaries Instance;
+
+    private float originalCameraAspect;
+
+    private void Awake()
+    {
+
+        if (Instance == null)
+        {
+            Instance = this;
+
+        }
+        else if (Instance != this)
+        {
+            Destroy(Instance);
+        }
+
+        originalCameraAspect = Camera.main.aspect;
+        ScreenBounds = OrthographicBounds();
+
+    }
+
+
+    private void Update()
+    {
+
+        if (originalCameraAspect != Camera.main.aspect)
+        {
+
+            ScreenBounds = OrthographicBounds();
+            originalCameraAspect = Camera.main.aspect;
+
+        }
+
+    }
+
+    private Bounds OrthographicBounds()
+    {
+        float screenAspect = (float)Screen.width / (float)Screen.height;
+        float cameraHeight = Camera.main.orthographicSize * 2;
+        Bounds bounds = new Bounds(
+            Camera.main.transform.position,
+            new Vector3(cameraHeight * screenAspect, cameraHeight, 0));
+        return bounds;
+    }
+
+
+
+}
